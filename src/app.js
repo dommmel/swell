f204bfb7dbb43fdebe67e6638750705ac1b385b0
Dommmel <dommmel@gmail.com>
2021-11-05 22:23:40 +0100

    replace glsl-shader-loader with glslify-loader

--- src/app.js
+++ src/app.js
@@ -16,28 +16,8 @@ gl.clearColor(0, 0, 0, 1);
 // SET UP PROGRAM
 /////////////////////
 
-var vsSource = `#version 300 es
-
-layout (location=0) in vec4 position;
-layout (location=1) in vec3 color;
-
-out vec3 vColor;
-
-void main() {
-
-    vColor = color;
-    gl_Position = position;
-}`;
-
-var fsSource = `#version 300 es
-precision highp float;
-
-in vec3 vColor;
-out vec4 fragColor;
-
-void main() {
-    fragColor = vec4(vColor, 1.0);
-}`;
+import fsSource from './fragmentShaderSource.glsl';
+import vsSource from './vertexShaderSource.glsl';
 
 var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, vsSource);
